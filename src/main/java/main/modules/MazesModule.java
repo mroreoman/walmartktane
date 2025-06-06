7d3f5d275f70b9a9a2813c8ff53c0e9678341564
mroreoman <830aryans@gmail.com>
2025-06-06 03:23:45 -0400

    deactivate buttons after module solved

--- src/main/java/main/modules/MazesModule.java
+++ src/main/java/main/modules/MazesModule.java
@@ -1,4 +1,4 @@
-package main.modules; //TODO possibly deactivate buttons after module solved
+package main.modules;
 
 import java.util.Random;
 
@@ -24,32 +24,32 @@ public class MazesModule extends ModuleBase {
   private static final double TRIANGLE_HEIGHT = Math.sqrt(Math.pow(15,2)-Math.pow(7.5,2));
   private static final double ARROW_SIZE = 30;
 
-  private final String[][] maze1 = {{"dr","lr","ld","dr","lr","l"}, {"ud*","dr","ul","ur","lr","ld"}, {"ud","ur","ld","dr","lr","uld*"}, {"ud","r","ulr","ul","r","uld"}, {"udr","lr","ld","dr","r","ud"}, {"ur", "l", "ur", "ul", "r", "ul"}};
-  private final String[][] maze2 = {{"r","ldr","l","dr","ldr","l"}, {"dr","ul","dr","ul","ur*","ld"}, {"ud","dr","ul","dr","lr","uld"}, {"udr","ul*","dr","ul","d","ud"}, {"ud","d","ud","dr","ul","ud"}, {"u","ur","ul","ur","lr","ul"}};
-  private final String[][] maze3 = {{"dr","lr","ld","d","dr","ld"}, {"u","d","ud","ur","ul","ud"}, {"dr","uld","ud","dr","ld","ud"}, {"ud","ud","ud","ud*","ud","ud*"}, {"ud","ur","ul","ud","ud","ud"}, {"ur","lr","lr","ul","ur","ul"}};
-  private final String[][] maze4 = {{"dr*","ld","r","lr","lr","ld"}, {"ud","ud","dr","lr","lr","uld"}, {"ud","ur","ul","dr","lr","ud"}, {"ud*","r","lr","ulr","lr","uld"}, {"udr","lr","lr","lr","ld","ud"}, {"ur","lr","l","r","ul","u"}};
-  private final String[][] maze5 = {{"r","lr","lr","lr","ldr","ld"}, {"dr","lr","lr","ldr","ul","u"}, {"udr","ld","r","ul","dr*","ld"}, {"ud","ur","lr","ld","u","ud"}, {"ud","dr","lr","ulr","l","ud"}, {"u","ur","lr","lr*", "lr","ul"}};
-  private final String[][] maze6 = {{"d","dr","ld","r","ldr*","ld"}, {"ud","ud","ud","dr","ul","ud"}, {"udr","ul","u","ud","dr","ul"}, {"ur","ld","dr","uld","ud","d"}, {"dr","ul","u*","ud","ur","uld"}, {"ur","lr","lr","ul","r","ul"}};
-  private final String[][] maze7 = {{"dr","lr*","lr","ld","dr","ld"}, {"ud","dr","l","ur","ul","ud"}, {"ur","ul","dr","l","dr","ul"}, {"dr","ld","udr","lr","ul","d"}, {"ud","u","ur","lr","ld","ud"}, {"ur","lr*","lr","lr","ulr","ul"}};
-  private final String[][] maze8 = {{"d","dr","lr","ld*","dr","ld"}, {"udr","ulr","l","ur","ul","ud"}, {"ud","dr","lr","lr","ld","ud"}, {"ud","ur","ld*","r","ulr","ul"}, {"ud","d","ur","lr","lr","l"}, {"ur","ulr","lr","lr","lr","l"}};
-  private final String[][] maze9 = {{"d","dr","lr","lr","ldr","ld"}, {"ud","ud","dr*","l","ud","ud"}, {"udr","ulr","ul","dr","ul","ud"}, {"ud","d","dr","ul","r","uld"}, {"ud*","ud","ud","dr","ld","u"}, {"ur","ul","ur","ul","ur","l"}};
-
-  private final String[][][] MAZES = {maze1, maze2, maze3, maze4, maze5, maze6, maze7, maze8, maze9};
+  private static final String[][] maze1 = {{"dr","lr","ld","dr","lr","l"}, {"ud*","dr","ul","ur","lr","ld"}, {"ud","ur","ld","dr","lr","uld*"}, {"ud","r","ulr","ul","r","uld"}, {"udr","lr","ld","dr","r","ud"}, {"ur", "l", "ur", "ul", "r", "ul"}};
+  private static final String[][] maze2 = {{"r","ldr","l","dr","ldr","l"}, {"dr","ul","dr","ul","ur*","ld"}, {"ud","dr","ul","dr","lr","uld"}, {"udr","ul*","dr","ul","d","ud"}, {"ud","d","ud","dr","ul","ud"}, {"u","ur","ul","ur","lr","ul"}};
+  private static final String[][] maze3 = {{"dr","lr","ld","d","dr","ld"}, {"u","d","ud","ur","ul","ud"}, {"dr","uld","ud","dr","ld","ud"}, {"ud","ud","ud","ud*","ud","ud*"}, {"ud","ur","ul","ud","ud","ud"}, {"ur","lr","lr","ul","ur","ul"}};
+  private static final String[][] maze4 = {{"dr*","ld","r","lr","lr","ld"}, {"ud","ud","dr","lr","lr","uld"}, {"ud","ur","ul","dr","lr","ud"}, {"ud*","r","lr","ulr","lr","uld"}, {"udr","lr","lr","lr","ld","ud"}, {"ur","lr","l","r","ul","u"}};
+  private static final String[][] maze5 = {{"r","lr","lr","lr","ldr","ld"}, {"dr","lr","lr","ldr","ul","u"}, {"udr","ld","r","ul","dr*","ld"}, {"ud","ur","lr","ld","u","ud"}, {"ud","dr","lr","ulr","l","ud"}, {"u","ur","lr","lr*", "lr","ul"}};
+  private static final String[][] maze6 = {{"d","dr","ld","r","ldr*","ld"}, {"ud","ud","ud","dr","ul","ud"}, {"udr","ul","u","ud","dr","ul"}, {"ur","ld","dr","uld","ud","d"}, {"dr","ul","u*","ud","ur","uld"}, {"ur","lr","lr","ul","r","ul"}};
+  private static final String[][] maze7 = {{"dr","lr*","lr","ld","dr","ld"}, {"ud","dr","l","ur","ul","ud"}, {"ur","ul","dr","l","dr","ul"}, {"dr","ld","udr","lr","ul","d"}, {"ud","u","ur","lr","ld","ud"}, {"ur","lr*","lr","lr","ulr","ul"}};
+  private static final String[][] maze8 = {{"d","dr","lr","ld*","dr","ld"}, {"udr","ulr","l","ur","ul","ud"}, {"ud","dr","lr","lr","ld","ud"}, {"ud","ur","ld*","r","ulr","ul"}, {"ud","d","ur","lr","lr","l"}, {"ur","ulr","lr","lr","lr","l"}};
+  private static final String[][] maze9 = {{"d","dr","lr","lr","ldr","ld"}, {"ud","ud","dr*","l","ud","ud"}, {"udr","ulr","ul","dr","ul","ud"}, {"ud","d","dr","ul","r","uld"}, {"ud*","ud","ud","dr","ld","u"}, {"ur","ul","ur","ul","ur","l"}};
+  private static final String[][][] MAZES = {maze1, maze2, maze3, maze4, maze5, maze6, maze7, maze8, maze9};
 
   private final Cell[][] cells = new Cell[6][6];
   private final int mazeIndex;
   private final Cell finish;
-
   private Cell current;
-  private int curCol;
-  private int curRow;
   
   private abstract class Cell extends StackPane {
     private final String directions;
+    private final int col;
+    private final int row;
 
     public Cell(int col, int row) {
       super();
       this.directions = MAZES[mazeIndex][col][row];
+      this.col = col;
+      this.row = row;
 
       setMinSize(CELL_SIZE, CELL_SIZE);
       setMaxSize(CELL_SIZE,CELL_SIZE);
@@ -118,7 +118,7 @@ public class MazesModule extends ModuleBase {
     public void deselect() {}
   }
 
-  private class Arrow extends Button {
+  private class Arrow extends Button { //FIXME arrows squish when clicked
     String direction;
 
     public Arrow(String direction) {
@@ -136,8 +136,10 @@ public class MazesModule extends ModuleBase {
     }
 
     private void move() {
-      int targetCol = curCol;
-      int targetRow = curRow;
+      if (current == finish) return;
+
+      int targetCol = current.col;
+      int targetRow = current.row;
       switch (direction) {
         case "u" -> targetCol--;
         case "l" -> targetRow--;
@@ -163,23 +165,23 @@ public class MazesModule extends ModuleBase {
         cells[i][j] = new NormalCell(i, j);
       }
     }
-
-    moveCursorTo(rand.nextInt(6), rand.nextInt(6));
-
-    // pick random finCol, finRow that is not the same as curCol, curRow
     int finCol = rand.nextInt(6);
-    int finRow;
-    if (finCol == curCol) {
-      finRow = rand.nextInt(5);
-      if (finRow >= curRow) {
-        finRow++;
-      }
-    } else {
-      finRow = rand.nextInt(6);
-    }
-
+    int finRow = rand.nextInt(6);
     cells[finCol][finRow] = new FinishCell(finCol, finRow);
     finish = cells[finCol][finRow];
+
+    // pick random curCol, curRow that is not the same as finCol, finRow
+    int curCol = rand.nextInt(6);
+    int curRow;
+    if (curCol == finCol) {
+      curRow = rand.nextInt(5);
+      if (curRow >= finRow) {
+        curRow++;
+      }
+    } else {
+      curRow = rand.nextInt(6);
+    }
+    moveCursorTo(curCol, curRow);
 
     initGUI();
   }
@@ -234,8 +236,6 @@ public class MazesModule extends ModuleBase {
   }
 
   private void moveCursorTo(int col, int row) {
-    curCol = col;
-    curRow = row;
     current = cells[col][row];
     current.select();
   }
